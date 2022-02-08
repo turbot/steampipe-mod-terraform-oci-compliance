fcc9736536ea971841922b731c672bb2abe54909
Arnab Ghosh <arnabg57@gmail.com>
2022-02-08 20:24:03 +0530

    add encryption related queries

---
+++ query/database/database_db_home_encryption_enabled.sql
@@ -0,0 +1,15 @@
+select
+  type || ' ' || name as resource,
+  case
+    when coalesce((arguments ->> 'kms_key_id'), '') = '' then 'alarm'
+    else 'ok'
+  end as status,
+  name || case
+    when coalesce((arguments ->> 'kms_key_id'), '') = '' then ' encryption disabled'
+    else ' encryption enabled'
+  end || '.' reason,
+  path
+from
+  terraform_resource
+where
+  type = 'oci_database_db_home';
